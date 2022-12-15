ca6a7604716d5a3630f9eb240e61d3e56403497c
Mike Oram <mike.oram@mayden.co.uk>
2022-12-15 16:48:48 +0000

    add public files

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 
 const app = express()
+
+// allows us to serve static files (images, css) from the public folder
+app.use(express.static('public'))
+
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 
 const port = 3000
+
+
 
 app.get('/', (req, res) => {
     const context = {title:"Front end rendered HTML!", content: "Using this format you can inject data into the page, usually this would come from a DB."}
